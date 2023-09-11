3794a3fae10792d8c49f5a82d948bdbdcc9db9bd
Johann Echavarria <johannechavarria@hotmail.com>
2023-09-11 01:30:37 -0500

    Comments update

--- src/findPairs/index.js
+++ src/findPairs/index.js
@@ -1,12 +1,16 @@
 /**
- * Function that finds pairs of integers from a list that sum to a given value
- * @param {Array<number>} numbers
- * @param {number} target
+ * @description Finds pairs of integers from a list that sum to a given value.
+ * @param {Array<number>} numbers - An array of integers.
+ * @param {number} target - The target sum value.
+ * @returns {Array<Array<number>>} An array of integer pairs that sum to the target value.
  */
 const findPairs = (numbers, target) => {
   const pairsFound = [];
+
+  // The set is the most efficient structure here for the hashing
   const numbersMapped =  new Set();
   for(let num of numbers){
+    // Calculating the pair upfront enhances the algorithm's efficiency in time and space, O(n) in both.
     const pairCandidate = target - num;
     if(numbersMapped.has(pairCandidate)){
       pairsFound.push([num, pairCandidate]);
